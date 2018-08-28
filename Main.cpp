0bbd2f4eb183cfda75329247ef0493663d6c75f5
liht <liht@LAPTOP-KINKHQ8U>
2018-08-28 19:58:13 +0800

    新建

---
+++ Main.cpp
@@ -0,0 +1,175 @@
+﻿
+#include "StringDemo.h"				// string 实例
+using namespace std;
+#include <string>
+
+
+#include <stdio.h>
+#include <windows.h>  
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+#define BUILDING_LIBCURL
+#define HTTP_ONLY
+#pragma comment(lib,"libcurl.lib")
+#pragma comment(lib,"winmm.lib")
+#pragma comment(lib,"wldap32.lib")
+#pragma comment(lib,"ws2_32.lib")
+#include <curl/curl.h>
+#include "assert.h"
+
+size_t Reply(void * ptr, size_t size, size_t nmemb, void * stream)
+{
+	string* str = (string*)stream;
+	(*str).append((char*)ptr, size*nmemb);
+	return size*nmemb;
+}
+wstring str_cvt(const string& from_str, int cvt_type) {
+	int wstr_len = MultiByteToWideChar(cvt_type, 0, from_str.c_str(), -1, nullptr, 0);
+	wchar_t *wstr = new wchar_t[wstr_len + 1];
+	memset(wstr, 0, (wstr_len + 1) * sizeof(wchar_t));
+	MultiByteToWideChar(cvt_type, 0, from_str.c_str(), -1, wstr, wstr_len);
+	wstring ret_str(wstr); delete[] wstr;
+	return ret_str;
+}
+string str_cvt(const wstring& from_str, int cvt_type) {
+	int str_len = WideCharToMultiByte(cvt_type, 0, from_str.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	char *str = new char[str_len + 1];
+	memset(str, 0, str_len + 1);
+	WideCharToMultiByte(cvt_type, 0, from_str.c_str(), -1, str, str_len, nullptr, nullptr);
+	string ret_str(str); delete[] str;
+	return ret_str;
+}
+
+#define  SKIP_PEER_VERIFICATION 1  
+//#define  SKIP_HOSTNAME_VERFICATION 1  
+
+/*
+ptr是指向存储数据的指针，
+size是每个块的大小，
+nmemb是指块的数目，
+stream是用户参数。
+所以根据以上这些参数的信息可以知道，ptr中的数据的总长度是size*nmemb
+*/
+size_t call_wirte_func(const char *ptr, size_t size, size_t nmemb, std::string *stream)
+{
+	assert(stream != NULL);
+	size_t len = size * nmemb;
+	stream->append(ptr, len);
+	return len;
+}
+// 返回http header回调函数    
+size_t header_callback(const char  *ptr, size_t size, size_t nmemb, std::string *stream)
+{
+	assert(stream != NULL);
+	size_t len = size * nmemb;
+	stream->append(ptr, len);
+	return len;
+}
+
+
+size_t req_reply(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	cout << "----->reply" << endl;
+	string *str = (string*)stream;
+	cout << *str << endl;
+	(*str).append((char*)ptr, size*nmemb);
+	return size * nmemb;
+}
+
+// http GET  
+CURLcode curl_get_req(const std::string &url, std::string &response)
+{
+	// init curl  
+	CURL *curl = curl_easy_init();
+	// res code  
+	CURLcode res;
+	if (curl)
+	{
+		// set params  
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // url  
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false); // if want to use https  
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false); // set peer and host verify false  
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+		curl_easy_setopt(curl, CURLOPT_READFUNCTION, NULL);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, req_reply);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response);
+		curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+		curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+		curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 3); // set transport and time out time  
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 3);
+		// start req  
+		res = curl_easy_perform(curl);
+	}
+	// release curl  
+	curl_easy_cleanup(curl);
+	return res;
+}
+
+// http POST  
+CURLcode curl_post_req(const string &url, const string &postParams, string &response)
+{
+	// init curl  
+	CURL *curl = curl_easy_init();
+
+	// res code  
+	CURLcode res;
+	if (curl)
+	{
+		// set params  
+		curl_easy_setopt(curl, CURLOPT_POST, 1); // post req  
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // url  
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postParams.c_str()); // params  
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0); // if want to use https  
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false); // set peer and host verify false  
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+		curl_easy_setopt(curl, CURLOPT_READFUNCTION, NULL);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, req_reply);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response);
+		curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+		curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+		curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 3);
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 3);
+		// start req  
+		res = curl_easy_perform(curl);
+	}
+	// release curl  
+	curl_easy_cleanup(curl);
+	return res;
+}
+
+
+int main(int arg , char ** args)
+{
+
+	// global init  
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	CURLcode res;
+	// test get requery  
+	string getUrlStr = "https://cn.bing.com/images/trending?form=Z9LH";
+	string getResponseStr;
+	res = curl_get_req(getUrlStr, getResponseStr);
+	if (res != CURLE_OK)
+		cerr << "curl_easy_perform() failed: " + string(curl_easy_strerror(res)) << endl;
+	else
+		cout << getResponseStr << endl;
+
+	// test post requery  
+// 	string postUrlStr = "https://www.baidu.com/s";
+// 	string postParams = "f=8&rsv_bp=1&rsv_idx=1&word=picture&tn=98633779_hao_pg";
+// 	string postResponseStr;
+// 	res = curl_post_req(postUrlStr, postParams, postResponseStr);
+// 	if (res != CURLE_OK)
+// 		cerr << "curl_easy_perform() failed: " + string(curl_easy_strerror(res)) << endl;
+// 	else
+// 		cout << postResponseStr << endl;
+
+	// global release  
+	curl_global_cleanup();
+	system("pause");
+	return 0;
+}
---
+++ StringDemo.cpp
@@ -0,0 +1,94 @@
+﻿#include "StringDemo.h"
+
+
+#include <string>
+using namespace std;
+void testfuctong1(string &t)		// string  作为参数 一般用引用
+{
+	return;	
+}
+void testfuctong2(const string &t)		// string  作为参数 一般用引用
+{
+	return;
+}
+
+void Demo_String()
+{
+	string str1("test");
+	string str2 = "test2";
+	string str3("12345678",3);
+
+
+	//获取字符串内部的char*
+	const char *p = str3.c_str();
+
+	string t1;
+	t1.append("something is");			//  附加 字符串
+	t1.append(" nothing!");
+
+	//考虑效率问题
+	string t2;
+	t2.resize(1024);		//调整大小
+	t2.clear();				// 清空 
+	t2.append("12");
+	t2.append("34");
+	t2.append("sdfadsfsf34",1,3); //  添加 dfa
+
+	string t3 = "Hello";
+	t3[1] = 'i';
+	t3.at(1) = 'i';  //at(n)表示返回下标为n的元素的引用。    效果同  t3[1] = 'i';
+
+	// 字符串的比较      string  重载了所有关系操作符（== ,!=, >= <=）
+	string t4 = "yes";
+	if (t4 == "yes")
+	{
+		printf("TRUE");
+	}
+
+	// 字符串找
+	string st5 = "liming is doing homework";
+	int pos = st5.find('i');
+	pos = st5.rfind('o');  // 存在返回未知  不存在-1
+	pos = st5.find("do");
+
+	pos = st5.find_first_of("23fdgh");		// 用于查找若干字符中的一个 ，只有其中任意一个字符匹配即返回。
+	pos = st5.find_first_not_of("234325rf"); // 和上面 相反
+
+	//返回子串
+	string t6("advasdsdaftt");
+	string r1 = t6.substr(4);  // 4后的所有串
+	string r2 = t6.substr(4, 2); // 4后的2个字符
+
+	//  replace  insert  string  不适宜进行插入和删除，涉及到 大规模数据的移动，如果频繁的使用 会比较消耗CPU
+
+
+
+	printf("%s", p);
+	return;
+}
+#include "vector"
+void vector_demo()
+{
+	vector <int>  arr(16);
+	arr.clear();
+
+	arr.push_back(1);		//capacity:16   size 1
+	arr.push_back(2);		//capacity 16   size 2
+	int ncapacity = arr.capacity();
+	int nsize = arr.size();
+
+	// 迭代器遍历
+	vector<int>::iterator iter;
+	for (iter = arr.begin();iter!=arr.end();iter++)
+	{
+		int &value = *iter;
+		printf("%d", value);
+	}
+
+	for (int i = 0; i<arr.size();i++)
+	{
+		int &value = arr[i];
+		printf("%d", value);
+	}
+
+}
